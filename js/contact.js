ac40960632e40614d97389b459b1fc27f0d2b67d
Alexis Abner <alexisabner@alexiss-air.lan>
2023-10-31 21:40:35 -0600

    Update contact.html and related css code

---
+++ js/contact.js
@@ -0,0 +1,5 @@
+(function() {
+    let form = document.querySelector("#register-form");
+    let emailInput = document.querySelector("#email");
+    let phoneInput = document.querySelector("#phone");
+})();
